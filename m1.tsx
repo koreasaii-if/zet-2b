3251769fd41e1a56a605effa1e2b3a0b1643cb10
koreasaii-if <77981074+koreasaii-if@users.noreply.github.com>
2021-03-01 23:21:58 +0100

    Add files via upload

---
+++ m1.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="m1" tilewidth="64" tileheight="64" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="64" source="../gar-1/m1.png"/>
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="../gar-1/m2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="../gar-1/m3.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="64" source="../gar-1/m4.png"/>
+ </tile>
+</tileset>
